af84ceecdf6eacf7886a27661cbf142e7c0a739c
Vitor Mateus <oliveirasantos.vm@gmail.com>
2020-10-05 22:54:49 -0300

    Create login page, and your style.

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,15 +2,25 @@ const user = require('../models/user');
 
 
 exports.login = (req, res) => {
-    
-    res.render('login', {title: 'GardenShop: Seja bem vindo!'});
+    res.render('login', { title: 'GardenShop: Seja bem vindo!' });
 }
+
+exports.post_login = (req, res) => {
+    res.render('signup', { title: 'GardenShop: Cadastre-se para ver mais!' });
+}
+
+exports.signup = (req, res) => {
+    res.render('signup', { title: 'GardenShop: Cadastre-se para ver mais!' });
+}
+
+exports.post_signup = (req, res) => {
+    res.render('signup', { title: 'GardenShop: Cadastre-se para ver mais!' });
+}
+
 exports.logout = (req, res) => {
     //aqui ele sai
 }
-exports.sigin = (req, res) => {
-   ender('sign', {title: 'GardenShop: Cadastre-se para ver mais!'});
-}
+
 exports.forgetPass = (req, res) => {
-    res.render('forgetpass', {title: 'GardenShop: ESQUEajudaremos você!'});
+    res.render('forgetpass', { title: 'GardenShop: ajudaremos você!' });
 }
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -5,10 +5,16 @@ const authController = require('../controllers/authController');
 
 router.get('/entrar', authController.login);
 
-router.get('/cadastrar', authController.sigin);
+router.get('/cadastrar', authController.signup);
 
 router.get('/sair', authController.logout);
 
 router.get('/esqueceuasenha', authController.forgetPass);
+
+router.post('/entrar', authController.post_login);
+
+router.post('/cadastrar', authController.post_signup);
+
+
 
 module.exports = router;
